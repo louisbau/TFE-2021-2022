4ae53df2190d025e300fb8f153257eeb2516c587
louisbau <bauchau.louis@gmail.com>
2022-02-20 17:44:34 +0100

    input button implémentation

---
+++ TFE/screens/ChatRoomScreen.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { Text, View, StyleSheet, FlatList, SafeAreaView } from 'react-native';
+import Message from "../components/Message";
+import ChatsData from "../assets/dummy-data/Chats";
+import MessageInput from "../components/MessageInput";
+
+
+export default function ChatRoomScreen() {
+    return (
+        <SafeAreaView style={styles.page}>
+            <FlatList 
+                data={ChatsData.messages}
+                renderItem={({item}) => <Message message={item} />}
+                inverted
+            />
+            <MessageInput />
+        </SafeAreaView>
+    )
+};
+
+const styles = StyleSheet.create({
+    page: {
+        backgroundColor: 'white',
+        flex: 1,
+    },
+});
