5604039b9e08271de3c63a81734df96e72ff253b
louisbau <bauchau.louis@gmail.com>
2022-05-29 16:15:47 +0200

    docker compose

--- backend/index.js
+++ backend/index.js
@@ -80,7 +80,7 @@ io.on("connection", (socket) => {
 });
 
 
-const PORT = process.env.PORT || 3001; 
+const PORT = process.env.PORT || 5000; 
 
 server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
